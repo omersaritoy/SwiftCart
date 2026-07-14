8e7e840727b7248e1febde294422b9501c3d3a3e
Ömer Sarıtoy <omer.saritoy@turkkep.com.tr>
2026-07-14 15:05:37 +0300

    added CartController

---
+++ src/main/java/com/cavcav/swiftcart/cart/controller/CartController.java
@@ -0,0 +1,9 @@
+package com.cavcav.swiftcart.cart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/carts")
+public class CartController {
+}
