6f321592003c6c30cf9c6641ee04edc9e5b7cdf2
Ömer Sarıtoy <fltepior@gmail.com>
2026-08-27 23:43:10 +0300

    update reviews

--- src/main/java/com/cavcav/swiftcart/review/model/Review.java
+++ src/main/java/com/cavcav/swiftcart/review/model/Review.java
@@ -1,5 +1,11 @@
 package com.cavcav.swiftcart.review.model;
 
+import com.cavcav.swiftcart.user.model.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 @Entity
-public class Review {
+@Table(name = "reviews")
+
+public class Review extends BaseEntity {
 }
